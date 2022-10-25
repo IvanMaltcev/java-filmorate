396f1618c6ea0b99676aa44dadc2235a7e922dec
Ivan <ivan777maltsev@yandex.ru>
2022-10-25 17:46:18 +0300

    "Добавлено поле для хранения друзей"

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,15 +1,15 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
-@AllArgsConstructor
 public class User {
 
     private int id;
@@ -21,4 +21,13 @@ public class User {
     private String name;
     @PastOrPresent (message = "Дата рождения не может быть в будущем.")
     private LocalDate birthday;
+    private Set<Integer> friends = new HashSet<>();
+
+    public User(int id, String email, String login, String name, LocalDate birthday) {
+        this.id = id;
+        this.email = email;
+        this.login = login;
+        this.name = name;
+        this.birthday = birthday;
+    }
 }
