1ea08f810007030574bcc2b6f66af34814a41a43
Ivan <ivan777maltsev@yandex.ru>
2022-10-26 17:53:36 +0300

    "Изменен и добавлен метод для обработки исключений"

--- src/main/java/ru/yandex/practicum/filmorate/handler/handlerExceptions.java
+++ src/main/java/ru/yandex/practicum/filmorate/handler/handlerExceptions.java
@@ -1,8 +1,13 @@
 package ru.yandex.practicum.filmorate.handler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -10,16 +15,20 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 
+import javax.validation.ConstraintViolationException;
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
 public class handlerExceptions extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler({ValidationException.class})
-    public ResponseEntity<Object> handleValidationException(final ValidationException exception, WebRequest request) {
+    @ExceptionHandler({IllegalArgumentException.class})
+    protected ResponseEntity<Object> handleIllegalArgumentException(
+            final IllegalArgumentException exception, WebRequest request) {
         log.error("Ошибка: {}", exception.getMessage(), exception);
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamp", LocalDateTime.now());
@@ -31,8 +40,35 @@ public class handlerExceptions extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 
+    @Override
+    protected @NotNull ResponseEntity<Object> handleMethodArgumentNotValid(
+            final @NotNull MethodArgumentNotValidException exception,
+                  @NotNull HttpHeaders headers,
+                  @NotNull HttpStatus status,
+                  @NotNull WebRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", status.value());
+        body.put("error", status.getReasonPhrase());
+        body.put("path", request.getDescription(false).replace("uri=", ""));
+        List<String> errors = exception.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(this::getErrorString).collect(Collectors.toList());
+        body.put("message", errors);
+        log.error("Ошибка: {}", errors, exception);
+        return new ResponseEntity<>(body, headers, status);
+    }
+
+    private String getErrorString(ObjectError error) {
+        if (error instanceof FieldError) {
+            return ((FieldError) error).getField() + ": " + error.getDefaultMessage();
+        }
+        return error.getDefaultMessage();
+    }
+
     @ExceptionHandler({NotFoundException.class})
-    public ResponseEntity<Object> handleValidationException(final NotFoundException exception, WebRequest request) {
+    protected ResponseEntity<Object> handleValidationException(final NotFoundException exception, WebRequest request) {
         log.error("Ошибка: {}", exception.getMessage(), exception);
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamp", LocalDateTime.now());
@@ -45,7 +81,7 @@ public class handlerExceptions extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler
-    public ResponseEntity<Object> handleOtherErrors(final Throwable exception, WebRequest request) {
+    protected ResponseEntity<Object> handleOtherErrors(final Throwable exception, WebRequest request) {
         log.error("Ошибка: {}", exception.getMessage(), exception);
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamp", LocalDateTime.now());
