afa44b7221399aacec8a66eec034b61b2e10028a
Ivan <ivan777maltsev@yandex.ru>
2022-10-26 17:59:49 +0300

    "Добавлены методы для обработки исключений"

--- src/main/java/ru/yandex/practicum/filmorate/handler/handlerExceptions.java
+++ src/main/java/ru/yandex/practicum/filmorate/handler/handlerExceptions.java
@@ -13,9 +13,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 
-import javax.validation.ConstraintViolationException;
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
 import java.util.List;
